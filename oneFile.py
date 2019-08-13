80cc3449b773c90ac94ce70c42500723dd3cdf98
JohnnyD5 <44303418+JohnnyD5@users.noreply.github.com>
2019-08-13 15:01:53 -0500

    base version draft

--- oneFile.py
+++ oneFile.py
@@ -13,53 +13,33 @@ pd.set_option('expand_frame_repr', True)
 class oneFile():
     def __init__(self, path = None):
         if path is None:
-            self.path = '/'
-        self.path = self.path + 'slice_data0'
-        pdf=pd.read_csv('slice_data0.csv', delimiter = ',', skiprows = 1,
-                        names = ["Q", "vx", "vy", "vz", "vx_avg", "vy_avg", "vz_avg",
-                                 "vx_prime", "vy_prime", "vz_prime", "vx_y",
-                                 "vy_z", "vz_x", "p", "p_avg", 'p_prime', "x", "y", "z"])
-        pdf.to_hdf('slice_data0.h5',key='df')
-        self.data = pd.read_hdf('slice_data0.h5', key='df')
+            self.path = 'D:/work/LidDrivenCavity/with_shashank/k_Re/case_k0.20_Re5/'
+        self.path = self.path + 'cloud.out'
+        pdf=pd.read_csv(self.path, delimiter = ' ', names = ["t", "x", "y", "z",
+                       "vx", "vy", "vz", "fx", "fy", "fz", "EulerAx", "EulerAy",
+                       "EulerAz", "wx", "wy", "wz", "Tx", "Ty", "Tz"]
+        pdf.to_hdf('cloud.h5',key='cloud')
+        self.data = pd.read_hdf('cloud.h5', key='cloud')
         self.data['v_mag'] = (self.data['vx']**2 + self.data['vy']**2)**0.5
-        self.data['vx_fluc'] = self.data['vx'] - self.data['vx_avg']
-        self.data['vy_fluc'] = self.data['vy'] - self.data['vy_avg']
-        self.data['v_fluc'] = (self.data['vx_fluc']**2 + self.data['vy_fluc']**2)**0.5
-        
-    def contour_plot_vmag(self, z = None, x = None, y = None):
-        if x is None:
-            x = self.data['x']
-        if y is None:
-            y = self.data['y']
-        if z is None:
-            z = self.data['v_mag']
-        fig, ax = plt.subplots(figsize=(22, 8))
-        #ax.tricontour(x, y, z, levels=14, linewidths=0.5, colors='k')
-        contour = ax.tricontourf(x, y, z, levels=200, cmap="rainbow")
-        #ax.scatter(x, y, z)
-        fig.colorbar(contour)
-        #ax2.set_title('tricontour (%d points)' % npts)
-        
-    def contour_plot_vfluc(self, z = None, x = None, y = None):
-        if x is None:
-            x = self.data['x']
-        if y is None:
-            y = self.data['y']
-        if z is None:
-            z = self.data['v_fluc']
-        fig, ax = plt.subplots(figsize=(22, 8))
-        #ax.tricontour(x, y, z, levels=14, linewidths=0.5, colors='k')
-        contour = ax.tricontourf(x, y, z, levels=200, cmap="rainbow")
-        #ax.scatter(x, y, z)
-        fig.colorbar(contour)
-        #ax2.set_title('tricontour (%d points)' % npts)
 
-#    def contour_plot_epsilon(self):
-#        # sort values in x axis
-#        self.data.sort_values('x', inplace = True)
-#        return 
-    
-    
+    def plot_angularV_over_t(self, t = None, y = None):
+        # Plot the angular velocity of the ellipse over time
+        if t is None:
+            x = self.data['t']
+        if y is None:
+            y = self.data['wz']
+        fig, ax = plt.subplots(figsize=(6, 5))
+        ax.plot(t, y)
+        ax.set_xlabel('t')
+        ax.set_ylabel(r'$\hat{\omega}$')
+        #removing top and right borders
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
+        # set x limit and y limit
+        ax.set_xlim(left = 0, right = 300)
+        plt.show()
+        return
+
     def test(self):
         self.path = self.path + 'slice_data0'
         print(self.path)
@@ -67,4 +47,4 @@ class oneFile():
 
 a = oneFile()
 print(a.data)
-a.contour_plot_vfluc()
+#a.contour_plot_vfluc()
