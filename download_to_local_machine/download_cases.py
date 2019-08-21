77df366f9a064d056a43c6b5bc0d6ed42319d15b
JohnnyD5 <44303418+JohnnyD5@users.noreply.github.com>
2019-08-21 14:54:09 -0500

    c

--- download_to_local_machine/download_cases.py
+++ download_to_local_machine/download_cases.py
@@ -11,13 +11,13 @@ for rhos in rhos_list:
         for AR in AR_list:
             for Re in Re_list:
                 casename = "case_rhos%.1f_k%.1f_AR%.1f_Re%.1f"%(rhos,k,AR,Re)
-
-        print(casename)
-        os.chdir(casename)
-        os.system('cp cloud.out ../download/%s/'% casename)
-        os.system('cp -r 0 ../download/%s/'% casename)
-        os.system('cp -r constant ../download/%s/'% casename)
-        os.system('cp -r system ../download/%s/'% casename)
-        os.system('cp solidDict ../download/%s/'% casename)
-        os.chdir('..')
+                print(casename)
+                os.chdir(casename)
+                os.system('cp cloud.out ../download/%s/'% casename)
+                os.system('cp -r 0 ../download/%s/'% casename)
+                os.system('cp -r constant ../download/%s/'% casename)
+                os.system('cp -r system ../download/%s/'% casename)
+                os.system('cp solidDict ../download/%s/'% casename)
+                os.system('cp view.foam ../download/%s/'% casename)
+                os.chdir('..')
 print("Program runs successfully!")
