8a1d8e32a4b74c07d1ad4876f15abdfe38ce81ec
JohnnyD5 <44303418+JohnnyD5@users.noreply.github.com>
2019-08-21 14:50:52 -0500

    c

--- download_to_local_machine/download_cases.py
+++ download_to_local_machine/download_cases.py
@@ -1,12 +1,17 @@
 import os
 
 # only change the list below
-k_list = [0.2, 0.33, 0.4, 0.5, 0.6]
-Re_list = [5, 6, 7 , 8, 10, 15, 20, 30, 40]
+rhos_list = [1.003]
+k_list = [0.2]
+AR_list = [0.5]
+Re_list = [7,7.2,7.3,7.4,7.6,7.8,8,8.5]
+###
+for rhos in rhos_list:
+    for k in k_list:
+        for AR in AR_list:
+            for Re in Re_list:
+                casename = "case_rhos%.1f_k%.1f_AR%.1f_Re%.1f"%(rhos,k,AR,Re)
 
-for k in k_list:
-    for Re in Re_list:
-        casename = "case_k%.2f_Re%1.f"%(k,Re)
         print(casename)
         os.chdir(casename)
         os.system('cp cloud.out ../download/%s/'% casename)
