0754e694c01ef802d4b6b3d08f2649a481255895
JohnnyD5 <44303418+JohnnyD5@users.noreply.github.com>
2019-08-23 10:57:41 -0500

    change structure

--- download_to_local_machine/download_cases.py
+++ download_to_local_machine/download_cases.py
@@ -1,5 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 23 10:52:44 2019
+
+@author: zding5
+"""
+
 import os
 
+os.system('mkdir download')
+os.chdir('download')
 # only change the list below
 rhos_list = [1.003]
 k_list = [0.2]
@@ -11,13 +20,12 @@ for rhos in rhos_list:
         for AR in AR_list:
             for Re in Re_list:
                 casename = "case_rhos%.1f_k%.1f_AR%.1f_Re%.1f"%(rhos,k,AR,Re)
-                print(casename)
-                os.chdir(casename)
-                os.system('cp cloud.out ../download/%s/'% casename)
-                os.system('cp -r 0 ../download/%s/'% casename)
-                os.system('cp -r constant ../download/%s/'% casename)
-                os.system('cp -r system ../download/%s/'% casename)
-                os.system('cp solidDict ../download/%s/'% casename)
-                os.system('cp view.foam ../download/%s/'% casename)
-                os.chdir('..')
+                print("generate case: ", casename)
+                os.system('mkdir %s' % casename)
+                os.system('cp ../%s/cloud.out /%s/'%(casename, casename))
+                #os.system('cp -r 0 ../download/%s/'% casename)
+                #os.system('cp -r constant ../download/%s/'% casename)
+                #os.system('cp -r system ../download/%s/'% casename)
+                #os.system('cp solidDict ../download/%s/'% casename)
+                #os.system('cp view.foam ../download/%s/'% casename)
 print("Program runs successfully!")
