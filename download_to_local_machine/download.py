652705bd7de561c3d088308270ab17060502d56c
JohnnyD5 <44303418+JohnnyD5@users.noreply.github.com>
2019-09-16 13:54:23 -0500

    Update download.py

--- download_to_local_machine/download.py
+++ download_to_local_machine/download.py
@@ -11,20 +11,23 @@ os.system('mkdir download')
 os.chdir('download')
 # only change the list below
 rhos_list = [1.003]
-k_list = [0.2]
-AR_list = [0.5]
-Re_list = [7,7.2,7.3,7.4,7.6,7.8,8,8.5]
+k_list = [0.2, 0.33, 0.4]
+#k_list =[0.2]
+AR_list = [0.25, 0.33, 0.5, 0.65, 0.75]
+#AR_list = [0.5]
+Re_list = [7, 8, 9, 10, 15, 20, 30]
+#Re_list = [7.0, 7.2, 7.4, 7.6, 7.8, 8.0]
 ###
 for rhos in rhos_list:
     for k in k_list:
         for AR in AR_list:
             for Re in Re_list:
-                casename = "case_rhos%.1f_k%.1f_AR%.1f_Re%.1f"%(rhos,k,AR,Re)
+                casename = "case_rhos%.1f_k%.2f_AR%.2f_Re%.1f"%(rhos,k,AR,Re)
                 print("generate case: ", casename)
                 os.system('mkdir %s' % casename)
                 os.system('cp ../%s/cloud.out %s/.'%(casename, casename))
                 os.system('cp -r ../%s/0 %s/.'%(casename, casename))
-                os.system('cp -r ../%s/300 %s/.'%(casename, casename))
+                os.system('cp -r ../%s/250 %s/.'%(casename, casename))
                 os.system('cp -r ../%s/constant %s/.'%(casename, casename))
                 os.system('cp -r ../%s/system %s/.'%(casename, casename))
                 os.system('cp ../%s/solidDict %s/.'%(casename, casename))
